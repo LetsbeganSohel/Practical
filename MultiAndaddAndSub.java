d2d7633ef4bcdb64359d925d02dfdbc123050f7a
Md Sohel Rana <110485029+LetsbeganSohel@users.noreply.github.com>
2022-08-09 17:32:54 +0530

    Add files via upload

---
+++ MultiAndaddAndSub.java
@@ -0,0 +1,14 @@
+class MultiAndaddAndSub{
+public static void main(String[] args){
+int a=13;
+int b=8;
+int multi =a*b;
+System.out.println("The multiplication is:" + multi);
+int sub =a-b;
+System.out.println("The subtraction  is:" + sub);
+int div=a/b;
+System.out.println("The Quotient is:" + div);
+int div2=a%b;
+System.out.println("The Remainder  is:" + div2);
+
+}}
