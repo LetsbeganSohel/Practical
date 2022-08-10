de0c351a04c31c20605623788c4f684eaa7c43cd
Md Sohel Rana <110485029+LetsbeganSohel@users.noreply.github.com>
2022-08-10 15:33:52 +0530

    Add files via upload

---
+++ Rectangle.java
@@ -0,0 +1,10 @@
+class Rectangle{
+public static void main(String[] args){
+double l =23.0;
+double w=15.5;
+double area =2*(l+w);
+double peri=l*w;
+System.out.println("Area of Rectangle is :" +area);
+System.out.println("Perimeter of Rectangle is :" +peri);
+}
+}
