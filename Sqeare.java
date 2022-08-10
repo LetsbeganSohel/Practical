85cd4935f8f48923dc2e44e4eebf76f680cc6a24
Md Sohel Rana <110485029+LetsbeganSohel@users.noreply.github.com>
2022-08-10 15:29:41 +0530

    Add files via upload

---
+++ Sqeare.java
@@ -0,0 +1,8 @@
+class Sqeare{
+public static void main(String [] args){
+int side=12;
+float area= side *side;
+int peri=4*side;
+System.out.println("Area of Squere is :"+area);
+System.out.println("Perimeter of Squere is :"+peri);
+}}
