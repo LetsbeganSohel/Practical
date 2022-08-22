8e729be4f28e0e34479fe5cafc5c582707b4b8ce
Md Sohel Rana <110485029+LetsbeganSohel@users.noreply.github.com>
2022-08-22 14:29:03 +0530

    Add files via upload

---
+++ Ifelseladder.java
@@ -0,0 +1,21 @@
+package com.company;
+
+public class Ifelseladder {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String name="Sohel";
+		if(name =="rony") {
+			System.out.println("NO! it is not");
+			
+		}
+		else if(name == "Sonjoy") {
+			System.out.println("no| it is not");
+		}
+		else {
+			System.out.println(name);
+		}
+
+	}
+
+}
