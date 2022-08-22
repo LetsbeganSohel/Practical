e31a7db53129b0fe37ebdf4400d107816ac39bf0
Md Sohel Rana <110485029+LetsbeganSohel@users.noreply.github.com>
2022-08-22 14:29:24 +0530

    Add files via upload

---
+++ Ifstatement.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class Ifstatement {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=4;
+		int b=5;
+		if(a+b<8) {
+			System.out.println("a+b is less than 10");
+		}
+		
+		else {
+			System.out.println("a+b is gratter ");
+		}
+
+	}
+
+}
