b435f7819854d5ebcb1cad169dfe553fba4b1bb0
Md Sohel Rana <110485029+LetsbeganSohel@users.noreply.github.com>
2022-08-23 17:55:59 +0530

    Add files via upload

---
+++ Pattarn2.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Pattarn2 {
+
+	public static void main(String[] args) {
+		for(int i=0;i<=9;i++) {
+			for(int j=1;j<=i;j++) {
+				System.out.print(i);
+			}
+			System.out.println("");
+		}
+
+
+	}
+
+}
